4b825d17ddbad4df0f2d60d725ca85c83afb8f9a
rajesh-zero <rajesh_zero@outlook.com>
2019-09-11 11:15:57 +0530

    type casting and variables

--- Python/shortPyPrograms/3.py
+++ Python/shortPyPrograms/3.py
@@ -8,3 +8,18 @@ print(type(x))
 print(type(y))
 print(type(b))
 print(type(c))
+
+#type casting
+x='5'
+y=5
+#print(x+y) gives error
+print(str(y)+x)
+print(int(x)+y)
+
+a,b='4','5'
+print(a+b)
+print(int(a+b)+4)
+
+
+i=input("enter something")
+print(i)

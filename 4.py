dd11980d2b5eefca3eef733d164b3dc4fe54357f
rajesh-zero <rajesh_zero@outlook.com>
2019-09-11 11:36:37 +0530

    String datatype

---
+++ 4.py
@@ -0,0 +1,12 @@
+#String datatype
+
+#slicing
+mystr="hellO how are you bro kaisa chal raha hai zindagi main"
+
+print(mystr[1])#it takes second letter
+print(mystr[::-1])
+print(len(mystr))#length of mystr
+print(mystr[1:5:])
+
+print(mystr.split('l'))
+print(mystr.swapcase())

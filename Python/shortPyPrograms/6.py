feadd0c83a8e43417309b1749f6fe60500dd9651
rajesh-zero <rajesh_zero@outlook.com>
2019-09-11 12:07:15 +0530

    dictionary

---
+++ Python/shortPyPrograms/6.py
@@ -0,0 +1,11 @@
+# Dictionary is nothing but key value pairs
+d1={"prasad":"indian","rajesh":"nepali","vipul":"chutiya"}
+# print(d1)
+# print(d1["rajesh"])
+
+for key in d1:
+    print(key)
+print("")
+
+for key,value in d1.items():
+    print(key,value)

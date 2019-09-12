8c7979c921ef08e01d77e48bf0c098ff0da0e295
rajesh-zero <rajesh_zero@outlook.com>
2019-09-12 11:41:49 +0530

    doctionary edit

--- Python/shortPyPrograms/6.py
+++ Python/shortPyPrograms/6.py
@@ -1,11 +1,21 @@
 # Dictionary is nothing but key value pairs
 d1={"prasad":"indian","rajesh":"nepali","vipul":"chutiya"}
+
+
+x=input("enter key")
+try:
+    (d1[x])
+except Exception as e:
+    print(e,"not found")
+#print(d1[x])
 # print(d1)
 # print(d1["rajesh"])
 
-for key in d1:
-    print(key)
-print("")
+# for key in d1:
+#     print(key)
+# print("")
 
-for key,value in d1.items():
-    print(key,value)
+# for key,value in d1.items():
+#     print(key,value)
+
+
---
+++ Python/shortPyPrograms/9.py
@@ -0,0 +1,17 @@
+l1=[1,2,3,4,5,6,7,8,9]
+if 5 in l1:
+    print("yes")
+
+# if condition:
+#     pass
+# else:
+#     pass
+
+
+x=int(input("enter your age"))
+if x<18:
+    print("you cannot drive")
+elif 20>x>18:
+    print("tere upar hai")
+else:
+    print("ja aag laga de")

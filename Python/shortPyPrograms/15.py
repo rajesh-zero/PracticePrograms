c08c64f9442bf56fcb308b1361418711a6773d45
rajesh-zero <rajesh_zero@outlook.com>
2019-09-23 23:56:25 +0530

    super samajh gaya almost

--- Python/shortPyPrograms/15.py
+++ Python/shortPyPrograms/15.py
@@ -1,1 +1,14 @@
 #overriding  super()
+class Funky:
+    def groove(self):
+        print("Hi in Funky")
+
+class Donkey(Funky):
+    def groove(self):
+        super().groove()# first it will run funky method after that it will run current method
+        print("Hi in donkey")
+    
+
+a_donkey = Donkey()
+a_donkey.groove()
+

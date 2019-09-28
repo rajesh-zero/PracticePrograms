52bbb0a1a130fc9ba27ac570a8e86e596d5e3ff3
rajesh-zero <rajesh_zero@outlook.com>
2019-09-28 13:13:20 +0530

    pickle module

--- Python/shortPyPrograms/21.py
+++ Python/shortPyPrograms/21.py
@@ -1,2 +1,9 @@
 #http module
-from 
+import requests
+r = requests.get("https://smartatentertainment.blogspot.com/")
+#print(r.text)
+with open("abc.html","w") as f:
+    f.write(r.text)
+    f.write("hii delete hua ki nai")
+with open("abc.html","w") as f:
+    f.write("hii delete hua ki nai")
